a3f5031cee89f56b7fcf776f8aee12f500775155
Stan Bai <SBai@bridgetree.com>
2016-08-04 15:23:30 +0800

    create a MVC5 project

---
+++ WebGarden/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebGarden.Startup))]
+namespace WebGarden
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
